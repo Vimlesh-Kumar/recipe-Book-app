839d12b30153fbc1cad701b926fa59236e87483e
Vimlesh-Kumar <vimlesh11072000@gmail.com>
2023-03-14 18:27:34 +0530

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './mycomponents/header/header.component';
+import { RecipesComponent } from './mycomponents/recipes/recipes.component';
+import { RecipeDetailsComponent } from './mycomponents/recipes/recipe-details/recipe-details.component';
+import { RecipeListComponent } from './mycomponents/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './mycomponents/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './mycomponents/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './mycomponents/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailsComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
