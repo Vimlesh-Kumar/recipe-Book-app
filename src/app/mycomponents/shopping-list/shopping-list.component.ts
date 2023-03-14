05e06c3b7b9aa708c1899b278ffdcf042b7130bc
Vimlesh-Kumar <vimlesh11072000@gmail.com>
2023-03-14 18:28:40 +0530

    my components

---
+++ src/app/mycomponents/shopping-list/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
+  ingredients:Ingredient[]=[
+    new Ingredient('apples',80),
+    new Ingredient('tomato',8)
+  ];
+}
